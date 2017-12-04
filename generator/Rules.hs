03577da958746b8fca2ffb8a4366bae449dcbd43
Mihai Maruseac <mihai.maruseac@gmail.com>
2017-12-03 19:53:52 -0800

    Cleanup old rules.

--- generator/Rules.hs
+++ generator/Rules.hs
@@ -14,32 +14,6 @@ module Rules (siteRules) where
 
 import Hakyll
 
-import Compilers
-
 -- | The rules used to build the site
 siteRules :: Rules ()
-siteRules = do
-  match "index.html" $ indexRules
-  match "posts/*" $ postRules
-  match "templates/*" $ templateRules
-  match "css/*" $ cssRules
-  match "javascript/*" $ idRules
-  match "images/*" $ idRules
-  match "fonts/*" $ idRules
-
-indexRules :: Rules ()
-indexRules = route idRoute >> compile indexCompiler
-
-cssRules :: Rules ()
-cssRules = route idRoute >> compile compressCssCompiler
-
-idRules :: Rules ()
-idRules = route idRoute >> compile copyFileCompiler
-
-templateRules :: Rules ()
-templateRules = compile templateCompiler
-
-postRules :: Rules ()
-postRules = do
-  route $ setExtension "html"
-  compile postCompiler
+siteRules = undefined
