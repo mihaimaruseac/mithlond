f537d0070cff6dcae76a74c9b0aa7fff2267b546
Mihai Maruseac <mihai.maruseac@gmail.com>
2017-12-15 17:28:21 -0800

    Refactor things around.

--- generator/Compilers.hs
+++ generator/Compilers.hs
@@ -19,14 +19,25 @@ import Text.Pandoc
 
 postCompiler :: Compiler (Item String)
 postCompiler = do
-  b <- pandocCompilerWithTransformM ro wo f
-  applyTemplate (readTemplate templateString) context b
+  b <- pandocCompilerWithTransformM readOptions writeOptions f
+  applyTemplate (readTemplate templateString) postContext b
   where
-    context = bodyField "body" `mappend` missingField
-    templateString = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /></head><body>$body$</body></html>"
-    ro = defaultHakyllReaderOptions
-    wo = defaultHakyllWriterOptions
     f :: Pandoc -> Compiler Pandoc
     f p = do
       unsafeCompiler $ print p
       return p
+
+readOptions :: ReaderOptions
+readOptions = defaultHakyllReaderOptions
+
+writeOptions :: WriterOptions
+writeOptions = defaultHakyllWriterOptions
+
+postContext :: Context String
+postContext = mconcat
+  [ bodyField "body"
+  , missingField
+  ]
+
+templateString :: String
+templateString = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /></head><body>$body$</body></html>"
