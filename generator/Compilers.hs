eff5abb218055bcbd2f8b6569a52fca7ec47f1ac
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-01-12 16:39:31 -0800

    Make module headers be truthful.

--- generator/Compilers.hs
+++ generator/Compilers.hs
@@ -1,11 +1,11 @@
 {-|
 Module: Compilers
-Description: The rules used to generate various pages on the site.
+Description: The compilers used to generate various pages on the site.
 Copyright: (c) Mihai Maruseac 2018
 Stability: experimental
 Portability: POSIX
 
-The rules used to generate various pages on the site.
+The compilers used to generate various pages on the site.
 -}
 
 module Compilers
