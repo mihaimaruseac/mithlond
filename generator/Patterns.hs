0bba3980967a806cb2a17bf53b73204899041d56
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-01-23 20:38:55 -0800

    Add new module for patterns to match on.

---
+++ generator/Patterns.hs
@@ -0,0 +1,26 @@
+{-|
+Module: Patterns
+Description: Patterns used for matching in rules and loading contexts
+Copyright: (c) Mihai Maruseac 2018
+Stability: experimental
+Portability: POSIX
+
+Patterns used for matching in rules and loading contexts. Needed because
+contexts sometimes need to load resources from some patterns, in order to
+follow the DRY principle.
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Patterns where
+
+import Hakyll
+
+patternPosts :: Pattern
+patternPosts = fromRegex "^posts/-?[0-9]+/.*\\.md$"
+
+patternTemplates :: Pattern
+patternTemplates = "templates/*"
+
+patternIndex :: Pattern
+patternIndex = "index.html"
