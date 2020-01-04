eee2535e1ed3b5a9082c4e45e5f5f54af74d1a1b
Mihai Maruseac <mihai.maruseac@gmail.com>
2020-01-04 14:35:27 -0700

    Finish documenting `main.hs`

--- generator/site.hs
+++ generator/site.hs
@@ -6,6 +6,10 @@ Stability: experimental
 Portability: POSIX
 
 The entry point of the site generator.
+
+This file just creates the `main` entry point by delegating to Hakyll's one
+(`hakyll`). The only changed parts are the site rules which are defined
+elsewhere.
 -}
 
 import Hakyll
